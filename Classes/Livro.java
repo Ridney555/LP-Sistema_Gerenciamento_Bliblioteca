b58e5f264b1de9f6062421fabf92b4cab8533097
Ridney <ralves.l24@us.edu.cv>
2026-06-18 16:40:37 -0100

    first commit

---
+++ Classes/Livro.java
@@ -0,0 +1,18 @@
+package Classes;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private int ano;
+    private String autor;
+
+    //contrutores
+    public Livro(int id, String titulo, int ano, String autor){
+        this.id = id;
+        this.titulo = titulo;
+        this.ano = ano;
+        this.autor = autor;
+    }
+
+    
+}
---
+++ Classes/Usuario.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private int telefone;
+    private String email;
+    
+    public Usuario(int id, String nome, int telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
